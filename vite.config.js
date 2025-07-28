1eea67a7c249885bdb11ec8a3c65a345b3188f16
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-07-28 16:15:39 +0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/tai-khan-partnership/',        // ← add this
   plugins: [react()],
   server: { port: 3000 },
-  assetsInclude: ['**/*.JPG']
+  assetsInclude: ['**/*.jpg']
 })
