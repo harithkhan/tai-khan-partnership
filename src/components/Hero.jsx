0b8bc117746ec4273cd1dd9f3e3bdc073d77d205
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-07-29 11:38:34 +0800

    change hero to KLCC

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,22 @@
+// src/components/Hero.jsx
 import React from 'react'
-import heroImage from '../assets/hero3.jpg'
+import heroImage from '../assets/hero4.jpg'
 
 export default function Hero() {
   return (
-    <section id="home" className="hero" style={{ backgroundImage: `url(${heroImage})` }}>
+    <section
+      id="home"
+      className="hero"
+      style={{
+        backgroundImage: `url(${heroImage})`,
+        backgroundPosition: 'top center',
+        backgroundSize: 'cover'
+      }}
+    >
       <div className="hero-text">
-        <h1>Tai & Khan Partnership</h1>
-        <h2>Your Reliable Lawyers</h2>
-        <h3>Every Call Returned. Every Detail Checked. Every Goal Met</h3>
+        <h1 style={{ color: 'var(--accent)' }}>Tai & Khan Partnership</h1>
+        <h2 style={{ color: 'var(--accent)' }}>Your Reliable Lawyers</h2>
+        <h3 style={{ color: 'var(--accent)' }}>Every Call Returned. Every Detail Checked. Every Goal Met</h3>
       </div>
     </section>
   )
